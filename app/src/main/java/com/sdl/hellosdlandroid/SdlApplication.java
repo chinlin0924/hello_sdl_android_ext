01047271a5423f7b29959e2a588f4aa2cceb3c49
Kujtim Shala <kshala@ford.com>
2016-03-10 11:49:30 +0100

    Initial commit with missing SdlService class. This class needs to get prepared to add features for each commit.

---
+++ app/src/main/java/com/sdl/hellosdlandroid/SdlApplication.java
@@ -0,0 +1,24 @@
+package com.sdl.hellosdlandroid;
+
+import android.app.Application;
+import android.content.Intent;
+
+public class SdlApplication extends Application{
+    private static SdlApplication instance;
+
+    public static SdlApplication getInstance() {
+        return instance;
+    }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        instance = this;
+
+        LockScreenActivity.registerActivityLifecycle(this);
+
+        Intent intent = new Intent(this, SdlService.class);
+        startService(intent);
+    }
+
+}
